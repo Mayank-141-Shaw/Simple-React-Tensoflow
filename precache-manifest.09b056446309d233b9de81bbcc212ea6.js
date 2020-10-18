9035b0b6b1f4bdc913d365eb5ae66d69e6163628
Mayank Shaw <mayank141shaw@gmail.com>
2020-10-18 21:22:44 +0530

    Making build

---
+++ precache-manifest.09b056446309d233b9de81bbcc212ea6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "218757b2961422051673a6c98f538a58",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7b6a1166ca187db4d2aa",
+    "url": "./static/css/main.07d54316.chunk.css"
+  },
+  {
+    "revision": "ae1cb07d094e92f1248e",
+    "url": "./static/js/2.3760ebeb.chunk.js"
+  },
+  {
+    "revision": "cb27756374c52bb13b029ba19a464106",
+    "url": "./static/js/2.3760ebeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b6a1166ca187db4d2aa",
+    "url": "./static/js/main.b353f4ac.chunk.js"
+  },
+  {
+    "revision": "63566a7510f037aac277",
+    "url": "./static/js/runtime-main.d5b71669.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
