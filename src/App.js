aee86347dd63ba515ba2fbf03341505642a8aef7
Mayank Shaw <mayank141shaw@gmail.com>
2020-10-18 21:14:36 +0530

    First version of tensorReact app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import TensorFlowExample from './components/TensorFlowExample';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TensorFlowExample/>
+    </div>
+  );
+}
+
+export default App;
